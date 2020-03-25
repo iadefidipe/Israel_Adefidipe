32b07398e3baa2ce796a03ea5af41496b2722edf
Israel Adefidipe <israeladefidipe@gmail.com>
2020-03-25 14:53:54 +0100

    Update Area_Calculator.py

--- Area_Calculator.py
+++ Area_Calculator.py
@@ -1,7 +1,8 @@
 # Code written by Israel Adefidipe
 # written on the 24/03/2020
 # A  program to that accepts a  user imputed radius as input and outputs the corresponding area
+import math
 print('Enter Desired radius(m): ')
 Desired_radius = float(input()) # Input function returns string values which is converted to numerical value with the float()
-Area = Desired_radius ** 2
+Area = math.pi * Desired_radius ** 2
 print('Area = ' + str(Area) + ' m²')
